438d243df7df01fb6bd54de9ae0ffddcc18a067b
imeureka <lky062@naver.com>
2024-07-08 02:29:43 +0900

    init : eslint, prettier 세팅

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,7 +1,7 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {StackMenu} from '@navigators/constants';
-import {StackParamList } from '@navigators/types';
+import {StackParamList} from '@navigators/types';
 import Home from '@screens/Home';
 
 const Stack = createNativeStackNavigator<StackParamList>();
