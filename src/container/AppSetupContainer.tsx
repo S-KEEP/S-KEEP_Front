c14fc459561c7512b9627d6968b155c4c736efc0
lky062@naver.com <lky062@naver.com>
2024-07-30 00:10:34 +0900

    [SKP-102] feat : recoil 세팅 및 api 인스턴스 생성

--- src/container/AppSetupContainer.tsx
+++ src/container/AppSetupContainer.tsx
@@ -1,11 +1,8 @@
 import React, {ReactNode} from 'react';
-import ReactQuerySetting from '@libs/react-query';
+import ReactQuerySetting from '../libs/react-query';
 
 type AppSetupWrapperProps = {children: ReactNode};
 
 export default function AppSetupWrapper({children}: AppSetupWrapperProps) {
-  return (
-      <ReactQuerySetting>{children}</ReactQuerySetting>
-
-  );
+  return <ReactQuerySetting>{children}</ReactQuerySetting>;
 }
---
+++ src/libs/recoil/constants/keys.ts
@@ -0,0 +1,30 @@
+export const enum RecoilStateKeys {
+  /** Token */
+  Token = 'token',
+  AccessToken = 'AccessToken',
+  RefreshToken = 'RefreshToken',
+
+  /** AuthState */
+  Auth = 'auth',
+
+  /** UserInfo */
+  UserInfo = 'userInfo',
+
+  /** Transaction */
+  Transaction = 'transaction',
+
+  /** Account */
+  Account = 'account',
+
+  /** Balance */
+  Balance = 'balance',
+
+  /** Category */
+  Category = 'category',
+
+  /** SelectFormBottomSheet */
+  SelectFormBottomSheet = 'selectFormBottomSheet',
+
+  /** SelectFormInput */
+  SelectFormInput = 'selectFormInput',
+}
