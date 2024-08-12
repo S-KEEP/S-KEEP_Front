b846ece15f607fcc1b0b6ac8f0e43ddf6c9f7967
Yooshin Kim <yusinkim.or@gmail.com>
2024-08-12 15:02:04 +0900

    [SKP-111] Revert "Revert "[SKP-11-image] 이미지 첨부 ""
    This reverts commit 6ece9c0faf658d5c49d0be7525c4afff0aff0e12.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,8 +2,22 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {Button, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StackScreenProps} from '../navigators/types';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 export default function Home({navigation}: StackScreenProps) {
+  async function goToImageLibrary() {
+    const {didCancel, errorCode, errorMessage, assets} =
+      await launchImageLibrary({
+        mediaType: 'photo',
+        selectionLimit: 10,
+      });
+
+    console.log(
+      `didCancel: ${didCancel} errorCode: ${errorCode} errorMessage: ${errorMessage}`,
+    );
+    console.log('>> ', assets);
+  }
+
   return (
     <View>
       <SafeAreaView>
@@ -12,6 +26,8 @@ export default function Home({navigation}: StackScreenProps) {
           title="Go to Analyze"
           onPress={() => navigation.navigate('Analyze')}
         />
+
+        <Button title="이미지 첨부(Max 10)" onPress={goToImageLibrary} />
       </SafeAreaView>
     </View>
   );
