d25ca70d5cd98f6dade6c5cbc5f473139a0f0d2b
lky062@naver.com <lky062@naver.com>
2024-08-13 13:22:15 +0900

    [SKP-127] feat : 인터셉터 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,13 +1,72 @@
 import axios, {AxiosError, AxiosResponse} from 'axios';
+import localStorage from '../libs/async-storage';
+import {TokenKeys} from '../libs/async-storage/constants/keys';
+import {includes} from 'lodash';
 
 export const baseURL = 'https://api.s-keep.site';
 
 export const axiosApi = axios.create({
   baseURL,
-  timeout: 10 * 1000,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
 });
+
+/**
+ *  헤더 토근 추가
+ */
+axiosApi.interceptors.request.use(
+  async config => {
+    const accessToken: string = await localStorage.get(TokenKeys.AccessToken);
+    console.log(accessToken);
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => Promise.reject(error),
+);
+
+/** 
+ *  Response Interceptor (응답 인터셉터)
+ *  1. onFulfilled
+ *  2. onRejected 
+ */
+const onFulfilled = (res: AxiosResponse) => {
+  return res;
+};
+
+const onRejected = async (error: AxiosError) => {
+  const originalConfig = error.config;
+
+  const refreshToken = (await localStorage.get(
+    TokenKeys.RefreshToken,
+  )) as string;
+
+  const statusArray = [4100, 4101, 4102, 4103, 4104];
+
+  if (originalConfig && includes(statusArray, error.response?.status)) {
+    try {
+      const response = await axiosApi.post('/api/auth/jwt/reissue', {
+        refreshToken,
+      });
+      const {result} = response.data.result;
+
+      localStorage.set(TokenKeys.AccessToken, result.accessToken);
+      localStorage.set(TokenKeys.RefreshToken, result.refreshToken);
+
+      originalConfig.headers.Authorization = `Bearer ${result.accessToken}`;
+
+      return axiosApi(originalConfig);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  }
+
+  return Promise.reject(error);
+};
+
+axiosApi.interceptors.response.use(onFulfilled, onRejected);
