300103d137f9d891127be2438ed82da208f048e4
lky062@naver.com <lky062@naver.com>
2024-08-23 17:53:41 +0900

    [SKP-148] chore : 쿼리키 추가

--- src/hooks/queries/category/useGetCategoryDetail.ts
+++ src/hooks/queries/category/useGetCategoryDetail.ts
@@ -1,7 +1,7 @@
 import {useInfiniteQuery} from '@tanstack/react-query';
 import {GET} from '../../../apis/client';
 import {useCallback} from 'react';
-import {LOCATION_KEYS} from '../QueryKeys';
+import {CATEGORY_KEYS} from '../QueryKeys';
 import {UserLocation} from '../../../types/dtos/location';
 import {Page} from '../../../types/dtos/category';
 
@@ -29,7 +29,7 @@ const getCategoryList = async ({userCategory, page}: GetPostHistoryRequest) => {
 
 const useGetCategoryList = (requestParams: GetPostHistoryRequest) => {
   const {data, hasNextPage, fetchNextPage, isFetching} = useInfiniteQuery({
-    queryKey: LOCATION_KEYS.list(requestParams),
+    queryKey: CATEGORY_KEYS.list(requestParams),
     queryFn: ({pageParam = 1}: {pageParam?: number}) =>
       getCategoryList({...requestParams, page: pageParam}),
     getNextPageParam: lastPage => lastPage.nextPage,
