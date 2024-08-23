88b95c06f180f299bc003e977e85827dda415823
lky062@naver.com <lky062@naver.com>
2024-08-24 01:31:39 +0900

    [SKP-155] style : 바텀탭 stroke 변경

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -37,5 +37,3 @@ export {default as IcCategoryRest} from './ic_category_rest2.svg';
 export {default as IcSad} from './ic_sad_face.svg';
 export {default as IcAppleLogo} from './ic_apple_logo.svg';
 export {default as IcAnalyze} from './ic_analyze.svg';
-
-
