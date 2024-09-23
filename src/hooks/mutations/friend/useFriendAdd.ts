191135e85c8746f80c8972077326656b7081f101
lky062@naver.com <lky062@naver.com>
2024-09-23 16:55:48 +0900

    [SKP-181] feat : 친구 초대 & 토큰 발급 API 수정

---
+++ src/hooks/mutations/friend/useFriendAdd.ts
@@ -0,0 +1,30 @@
+import {useMutation} from '@tanstack/react-query';
+import {BaseResponse, PATCH} from '../../../apis/client';
+
+interface FriendTokenPropRequest {
+  token: string;
+}
+
+/**
+ *  친구 추가하기
+ */
+export const pathchFriendAdd = async ({token}: FriendTokenPropRequest) => {
+  const res = await PATCH<string>(`/api/friend`, {token}); // 객체로 전달
+  return res.data;
+};
+
+interface PostInvitationTokenProps {
+  onSuccess: (res: BaseResponse<string>) => void;
+  onError: (e: Error) => void;
+}
+
+export const usePatchFriendAdd = ({
+  onSuccess,
+  onError,
+}: PostInvitationTokenProps) => {
+  return useMutation({
+    mutationFn: (req: FriendTokenPropRequest) => pathchFriendAdd(req),
+    onSuccess: onSuccess,
+    onError: onError,
+  });
+};
--- src/hooks/mutations/friend/usePostInvitationToken.ts
+++ src/hooks/mutations/friend/usePostInvitationToken.ts
@@ -1,17 +1,21 @@
 import {useMutation} from '@tanstack/react-query';
 import {BaseResponse, POST} from '../../../apis/client';
 
+export interface FriendTokenResponse {
+  friendToken: string;
+}
+
 /**
  *  친구 초대하기 토큰 발급
  */
 export const postInvitationToken = async () => {
-  const res = await POST<string>(`/api/friend/invite`);
-  console.log('친구 토큰 발급 호출 완료 : ', res.data);
+  const res = await POST<FriendTokenResponse>(`/api/friend/invite`);
+  console.log('친구 토큰 발급 호출 완료 : ', res.data.result.friendToken);
   return res.data;
 };
 
 interface PostInvitationTokenProps {
-  onSuccess: (res: BaseResponse<string>) => void;
+  onSuccess: (res: BaseResponse<FriendTokenResponse>) => void;
   onError: (e: Error) => void;
 }
 
