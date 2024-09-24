fdee7afd1af65e372af814e5114fccd7a427dd00
lky062@naver.com <lky062@naver.com>
2024-09-25 00:49:50 +0900

    [SKP-190] feat : 친구 상세 화면

---
+++ src/screens/Friend/Friend.style.ts
@@ -0,0 +1,46 @@
+import {StyleSheet} from 'react-native';
+import {theme} from '../../styles';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.palette.white,
+  },
+  FriendTopContainer: {
+    backgroundColor: theme.palette.primary,
+    paddingHorizontal: 28,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+  itemCount: {
+    padding: 15,
+    paddingLeft: 28,
+    ...theme.typography.title_m_16,
+  },
+  title: {
+    paddingTop: 29,
+    ...theme.typography.title_sb_21,
+  },
+  subTitle: {
+    paddingTop: 36,
+    ...theme.typography.sb_13,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+  },
+  categoryItem: {
+    gap: 6,
+    alignItems: 'center',
+    marginRight: 16,
+  },
+  categoryText: {
+    ...theme.typography.text_m_13,
+    color: theme.palette.gray6,
+  },
+});
